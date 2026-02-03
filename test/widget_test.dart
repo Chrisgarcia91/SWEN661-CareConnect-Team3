e6433a34c7793192cd0b56428d42d22270d3c1bf
Cristobal Garcia <cgarcia78@student.umgc.edu>
2026-02-02 18:48:06 -0800

    Applied theme logic to CareConnectApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:care_connect_team3/app/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CareConnectApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
